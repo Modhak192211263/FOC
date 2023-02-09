52f730bc70874ae36b5c8781325ab55aa21a6b01
Modhak192211263 <123918254+Modhak192211263@users.noreply.github.com>
2023-02-09 11:06:29 +0530

    Day1

---
+++ number series.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[20],i;
+	printf("enter n elements:");
+	scanf("%d",&n);
+	printf("elements are:");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("the number value is:");
+	for(i=0;i<n;i++)
+	printf("%d", a[i]);
+	getch();
+}
+
